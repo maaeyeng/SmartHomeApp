cf701971d5af5c497599e33c81be25b9e418e2a3
maaeyeng <maaeyeng@MacBook-Pro-khxng-maaeyeng-2.local>
2023-01-29 05:53:39 +0700

    add Smart Home V1

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text,View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.red}>Smart Home V1.0</Text>
+      <Image
+          source={{
+            uri: 'https://media.istockphoto.com/id/1218148871/vector/smart-home-emblem-for-digital-technologies-vector-illustration-eps-10.jpg?s=612x612&w=0&k=20&c=XBQEUXQu4VQhH6G-JgjGwUTiHsNqsL1UdA4PGWbvPig=',
+          }}
+          style={{width: 200, height: 200}}
+        />
+      <Button
+        onPress={() => {
+          console.log('You tapped the button!');
+        }}
+        title="Login"
+        color="#701809"
+      />
+
+
       <StatusBar style="auto" />
     </View>
+   
   );
 }
 
@@ -15,6 +31,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    titleText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
     justifyContent: 'center',
+    padding: 2,
   },
 });
---
+++ Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {
+    ListView,
+    Text,
+    View,
+
+} from 'react-native';
+
+class App extends Component {
+    
+
+    render() {
+        return (
+            <View>
+                <ListView
+
+                />
+            </View>
+        );
+    }
+}
+
+export default App;
